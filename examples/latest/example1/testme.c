5c186b5211c62d54e703f0bb62f92ef0f5f776eb
Omer Anson <oaanson@gmail.com>
2016-12-19 18:02:59 +0200

    Make example 1 compare 3 strings rather than 2

--- examples/latest/example1/testme.c
+++ examples/latest/example1/testme.c
@@ -29,20 +29,26 @@ int strcmp_wrapper(char * str1, char * str2) {
 }
 
 void testme() {
-	int tmp1, tmp2;
-	char *s1, * s2;
+	int tmp1, tmp2, tmp3;
+	char *s1, *s2, *s3;
 	char *const1 = "Hello World";
 	char *const2 = "Hello ESEC/FSE";
+	char *const3 = "How are you?";
 	s1 = klee_string(16, "s1");
 	s2 = klee_string(16, "s2");
+	s3 = klee_string(16, "s3");
 	tmp1 = strcmp_wrapper(s1,const1);
 	tmp2 = strcmp_wrapper(s2,const2);
+	tmp3 = strcmp_wrapper(s3,const3);
 	if (tmp1 == 0)
 	{
 		if (tmp2 == 0)
 		{
-			printf("Success");
-			__assert_fail("Success", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+			if (tmp3 == 0)
+			{
+				printf("Success");
+				__assert_fail("Success", __FILE__, __LINE__, __PRETTY_FUNCTION__);
+			}
 		}
 	}
 }
