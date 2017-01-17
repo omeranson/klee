d4c0d4c34afe284293105742ee0fc3780ffbd66b
Omer Anson <oaanson@gmail.com>
2017-01-17 23:53:25 +0200

    Improve getcwd example

--- examples/system/getcwd/getcwd.c
+++ examples/system/getcwd/getcwd.c
@@ -1,35 +1,41 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <assert.h>
 #include <limits.h>
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
 	char cwd[PATH_MAX];
-	getcwd(cwd, PATH_MAX);
+	cwd[10] = '/';
+	getcwd(cwd, 10);
+	assert(cwd[10] == '/');
+	if (strcmp(cwd, "") == 0) {
+		printf("I'm not anywhere...\n");
+		assert(0 && "I'm not anywhere...\n");
+		return 0;
+	}
+	if (cwd[0] != '/') {
+		printf("I'm relative!\n");
+		assert(0 && "I'm relative!\n");
+		return 0;
+	}
 	if (strcmp(cwd, "/") == 0) {
-		write(1, "I'm at root!\n", sizeof("I'm at root!\n")-1);
+		printf("I'm at root!\n");
+		assert(0 && "I'm at root!\n");
 		return 0;
 	}
 	if (strncmp(cwd, "/home", sizeof("/home")-1) == 0) {
-		write(1, "I'm at home!\n", sizeof("I'm at home!\n")-1);
+		printf("I'm at home!\n");
+		assert(0 && "I'm at home!\n");
 		return 0;
 	}
 	if (strncmp(cwd, "/tmp", sizeof("/tmp")-1) == 0) {
-		write(1, "I'm fleeting!\n", sizeof("I'm fleeting!\n")-1);
+		printf("I'm fleeting!\n");
+		assert(0 && "I'm fleeting!\n");
 		return 0;
 	}
-	write(1, "I'm... somewhere else!\n", sizeof("I'm... somewhere else!\n")-1);
-	/*
-	int len = strlen(cwd);
-	for (int idx = 0; idx < len; idx++) {
-		char c = cwd[idx];
-		if ((c >= 32) && (c <= 127)) {
-			printf("%c", c);
-		} else {
-			printf("%02x", c);
-		}
-	}
-	write(1, "\n", sizeof("\n")-1); */
+	printf("I'm... somewhere else!\n");
+	assert(0 && "I'm... somewhere else!\n");
 	return 0;
 }
 
